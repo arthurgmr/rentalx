5277f21672d260b21130e8fbda038c5d88975184
Arthur Machado <arthur.gmr@hotmail.com>
2021-08-02 22:35:58 -0300

    create-list-cars

---
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarRepositoryInMemory";
+
+import { ListCarsUseCase } from "./ListCarsUseCase";
+
+let listCarsUseCase: ListCarsUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("List Cars", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory);
+  });
+
+  it("Should be able to list all available cars", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car1",
+      description: "Car Description",
+      daily_rate: 300.0,
+      license_plate: "ABC-1234",
+      fine_amount: 100.0,
+      brand: "Car Brand",
+      category_id: "dccd220f-21ee-4888-9013-b292b2efc034",
+    });
+
+    const cars = await listCarsUseCase.execute({});
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("Should be able to list all available cars by brand name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car2",
+      description: "Car Description",
+      daily_rate: 300.0,
+      license_plate: "DEF-1234",
+      fine_amount: 100.0,
+      brand: "Car Brand Test",
+      category_id: "dccd220f-21ee-4888-9013-b292b2efc034",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      brand: "Car Brand Test",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("Should be able to list all available cars by name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car3 Test",
+      description: "Car Description",
+      daily_rate: 300.0,
+      license_plate: "GHI-1234",
+      fine_amount: 100.0,
+      brand: "Car Brand",
+      category_id: "dccd220f-21ee-4888-9013-b292b2efc034",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      name: "Car3 Test",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+});
