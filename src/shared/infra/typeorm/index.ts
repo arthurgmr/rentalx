4abe4f5d9382cc5c6231925bda571bda8de4ffd1
Arthur Machado <arthur.gmr@hotmail.com>
2021-11-30 11:31:17 -0300

    database-Docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -12,7 +12,22 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 //   });
 // });
 
-export default async (host = "database"): Promise<Connection> => {
+// using configs if applicantion be there in Docker;
+// export default async (host = "database"): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions();
+
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       host: process.env.NODE_ENV === "test" ? "localhost": host,
+//       database:
+//         process.env.NODE_ENV === "test"
+//           ? "rentx_test"
+//           : defaultOptions.database,
+//     })
+//   );
+// };
+
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
