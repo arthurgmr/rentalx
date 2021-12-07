ab4d094bc93671b5db6eb36c84e6e309a6c8807b
Arthur Machado <arthur.gmr@hotmail.com>
2021-12-07 18:00:49 -0300

    add-security

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,10 +5,14 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import "@shared/container";
 
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 
 import swaggerFile from "../../../swagger.json";
 import createConnection from "../typeorm";
@@ -16,6 +20,29 @@ import { router } from "./routes";
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -26,6 +53,18 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 429 and 500 errors
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
